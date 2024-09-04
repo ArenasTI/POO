97114e22b27467d0491f938a9d0f686c9f29eb35
Arenas Tiago Rodrigo <129696119+ArenasTI@users.noreply.github.com>
2024-09-04 12:48:59 -0300

    Ejercicios - Eclipse

---
+++ src/Ejercicio_1.java
@@ -0,0 +1,18 @@
+
+public class Ejercicio_1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("");
+
+	        int a = 10;
+	        int b = 5;
+
+	    System.out.println("Suma: " + (a + b));
+		System.out.println("Resta: " + (a - b));
+		System.out.println("Multiplicación: " + (a * b));
+		System.out.println("División: " + (a / b));
+		System.out.println("Módulo: " + (a % b));
+		
+	}
+}
---
+++ src/Ejercicio_12.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio_12 {
+
+	public static void main(String[] args) {
+
+		int num=1;
+
+		while (num<=100){
+			if (num%2==0 || num%3==0){
+				System.out.println(num);
+			}
+			num++;
+		}
+	}
+}
